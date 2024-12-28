1a3cd2eff71856e268bb4d9a1f795c01bcb3b411
Otabek.E <otabekjan.eshpulatov@gmail.com>
2024-12-28 21:37:36 +0100

    main page implemented

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,13 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-
-    const name = ref('John Doe');
-    const status = ref('active');
-    const tasks = ref(['Task one','Task two','Task three']);
-    const link = ref('https://google.com');
-
-    const newTask = ref('');
-
-    const toggleStatus = ()=>{
-      if(status.value == 'active'){
-        status.value = 'pending'
-      }else if(status.value == 'pending'){
-        status.value = 'inactive'
-      }else{
-        status.value = 'active'
-      }
-    };
-
-    const addTask = () =>{
-      if(newTask.value.trim() !== ''){
-        tasks.value.push(newTask.value.trim())
-        newTask.value = ''
-      }
-    }
-
-    const deleteTask = (index) =>{
-      tasks.value.splice(index,1)
-    }
-
-    onMounted(async () =>{
-      try{
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos');
-        const data = await response.json()
-        tasks.value = data.map((task)=>task.title);
-      }catch(error){
-        console.log('Error fetching tasks! '+ error)
-      }
-    })
-
-    
-
+import Navbar from './comonents/Navbar.vue';
+import Hero from './comonents/Hero.vue';
+import HomeCards from './comonents/HomeCards.vue';
+import JobListings from './comonents/JobListings.vue';
 </script>
 
 <template>
- <h1>{{name}}</h1>
- <p v-if="status == 'active'">User is active</p>
- <p v-else-if="status == 'pending'">User is pending</p>
- <p v-else>User is inactive</p>
-
- <form @submit.prevent="addTask">
-  <label for="newTask">Add Task</label>
-  <input type="text" id="newTask" name="newTask" v-model="newTask"/>
-  <button type="submit">Submit</button>
- </form>
-
- <h3>Tasks: </h3>
- <ul>
-  <li v-for="task,index in tasks" :key="task">
-    
-    <span>{{ task }}</span>
-    <button @click="deleteTask(index)">Delete</button>
-
-
-  </li>
- </ul>
-
- <a :href="link">Google</a>
- <br/>
- <!-- <button v-on:click="toggleStatus">Change status</button> -->
- <button @click="toggleStatus">Change status</button>
-
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings limit="3" show-button="true"/>
 </template>
-
-
